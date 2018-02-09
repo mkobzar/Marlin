55530c39c178704c4cdc236ae5bc16d65f918213
mike ko <kobzar@outlook.com>
2018-02-08 23:53:16 -0600

    update

--- Configuration_Ramps.h
+++ Configuration_Ramps.h
@@ -279,7 +279,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 6000, 6000, 300, 6000 }
+#define DEFAULT_MAX_FEEDRATE          { 3000, 3000, 5, 3000 }
 //#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
 
 
@@ -289,7 +289,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION  { 1500, 1500, 50, 5000 }//  { 500, 500, 50, 500 }  //{ 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION  { 1500, 1500, 10, 5000 }//  { 500, 500, 50, 500 }  //{ 3000, 3000, 100, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -299,9 +299,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1000    // 500     //3000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  1000    // 500     //3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   2000    // 500     //3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1000   // 500     //3000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  1000   // 500     //3000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1000   // 500     //3000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
